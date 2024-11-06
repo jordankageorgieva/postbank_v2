f7d356abb37af7f8e88544bee803e7a16ea4a151
jordankageorgieva <jo_georgieva@abv.bg>
2024-11-06 10:40:49 +0200

    code refactoring - remove component for data loading + data to be loaded only when the end point is access + ws data to be updated again when end point is accessed
    code refactoring - remove component for data loading + data to be loaded only when the end point is access + ws data to be updated again when end point is accessed

--- currenciesBNB/src/main/java/com/postbank/currenciesBNB/controller/CurrenciesController.java
+++ currenciesBNB/src/main/java/com/postbank/currenciesBNB/controller/CurrenciesController.java
@@ -1,36 +1,54 @@
 package com.postbank.currenciesBNB.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
 import com.postbank.currenciesBNB.model.Currencies;
 import com.postbank.currenciesBNB.repository.CurrenciesRepository;
+import com.postbank.currenciesBNB.service.BnbDataUpdate;
 
 @RestController
 public class CurrenciesController {
-	
+
+	@Autowired
+	BnbDataUpdate bnbDataUpdate;
+
 	private final CurrenciesRepository repository;
 
-    public CurrenciesController(CurrenciesRepository repository) {
-        this.repository = repository;
-    }
-	
-	@GetMapping("/download-currencies")
-    public  List<Currencies> displayDataCurrencies() {
+	public CurrenciesController(CurrenciesRepository repository) {
+		this.repository = repository;
+	}
 
+	@GetMapping("/download-currencies")
+	public List<Currencies> displayDataCurrencies() {
 		
-        //NOT WORK - BULLSHEET
-		/*
-		 * boolean rowUpdate =repository.updateRateByCode("AUD", 3.2); if (rowUpdate) {
-		 * System.out.println("row updatetd"); } else {
-		 * System.out.println("row NOT updatetd"); }
-		 */
-            
-        List<Currencies> curreencies= (List<Currencies>) repository.findAll();
-        
-        return curreencies;
-    }
-	
+		bnbDataUpdate.dataUpdate();
+
+		List<Currencies> curreencies = (List<Currencies>) repository.findAll();
+
+		return curreencies;
+	}
+
+
+//	@SendTo("/ws-download-currencies")
+//	public String greeting() throws Exception {
+//
+//	    bnbDataUpdate.dataUpdate();
+//
+//		Thread.sleep(1000); // simulated delay
+//
+//		// forword this data to WS
+//		Gson gson = new Gson();
+//		String jsonArray = gson.toJson(onlyUpdatedCurrencies);
+//
+//		return jsonArray;
+//	}
+
 }
